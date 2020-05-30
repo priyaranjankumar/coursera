12e08a9871b61ed70e957edb7c78f01c23faf47e
PriyaRanjan Kumar <mishrasubham00@gmail.com>
2020-05-31 04:24:29 +0530

    code added

---
+++ NodeJs/NodeExamples/index.js
@@ -0,0 +1,20 @@
+var rect ={
+        perimeter:(x,y)=>(2*(x+y)),
+        area:(x,y)=>(x*y)
+};
+
+function solveRect(l,b)
+{
+    console.log("Solving for rectange = "+ l + b);
+    if (l <=0 || b <= 0)
+    {
+        console.log("Rectangle Dimension should be greate than Zero")
+    }
+    else 
+        console.log("The area of the rectangle is  " + rect.area(l,b) )
+        console.log("The perimete of the rectangle is " + rect.perimeter(l,b))
+}
+
+solveRect(2,4);
+solveRect(0,5);
+solveRect(-3,5);
