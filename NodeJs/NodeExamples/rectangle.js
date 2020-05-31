de3c9b39b7e3cffe81efb7b322e5f0d9f08d2313
PriyaRanjan Kumar <mishrasubham00@gmail.com>
2020-05-31 12:38:49 +0530

    "Call Back added"

--- NodeJs/NodeExamples/rectangle.js
+++ NodeJs/NodeExamples/rectangle.js
@@ -1,2 +1,21 @@
-exports.perimeter = (x,y)=>(2*(x+y));
-exports.area = (x,y)=>(x*y);
+module.exports = (x,y,callback) =>{
+    if(x <= 0 || y <= 0)
+    {
+        setTimeout(()=> callback(new Error("Rectangle dimension are not correct"),null),2000);
+        
+    }
+    else
+   
+    {
+        setTimeout(()=> callback(null,{
+            perimeter = (x,y)=>(2*(x+y)),
+            area = (x,y)=>(x*y)
+        }),
+        2000);
+        
+    }
+}
+
+
+
+
